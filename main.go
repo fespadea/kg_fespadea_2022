5ab885eea5f76d82b36db995df34db6d35f13a31
Fernando Spadea <fespadearocks@gmail.com>
2021-05-03 03:28:17 -0400

    Feature completed, numbers converted to words

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,20 +14,43 @@ func main() {
 }
 
 func numbersAsWords(inputNumbers []string) string {
-	numberWords := ""
+	var numberWords strings.Builder // string builder is more efficient for concatenation
 	for i := 0; i < len(inputNumbers); i++ {
-		convertedNumber, err := strconv.Atoi(inputNumbers[i])
-		if err != nil {
-			return err.Error()
+		for j := 0; j < len(inputNumbers[i]); j++ {
+			convertedNumber, err := strconv.Atoi(inputNumbers[i][j : j+1]) // convert digit string to int
+			if err != nil {
+				return err.Error() // handle error if string wasn't a digit
+			}
+			fmt.Fprintf(&numberWords, convertDigitToWord(convertedNumber)) // add digit word to string
 		}
-		numberWords += convertNumberToWords(convertedNumber)
-		if i < len(inputNumbers)-1 {
-			numberWords += ","
-		}
+		fmt.Fprintf(&numberWords, ",") // separate separate numbers with commas
 	}
-	return numberWords
+	return numberWords.String()[:numberWords.Len()-1] // Don't want the last comma
 }
 
-func convertNumberToWords(inputNumber int) string {
-	return ""
+func convertDigitToWord(inputDigit int) string { // converts a single digit to its word equivalent
+	switch inputDigit {
+	case 0:
+		return "Zero"
+	case 1:
+		return "One"
+	case 2:
+		return "Two"
+	case 3:
+		return "Three"
+	case 4:
+		return "Four"
+	case 5:
+		return "Five"
+	case 6:
+		return "Six"
+	case 7:
+		return "Seven"
+	case 8:
+		return "Eight"
+	case 9:
+		return "Nine"
+	default:
+		return "INPUT_NOT_DIGIT"
+	}
 }
